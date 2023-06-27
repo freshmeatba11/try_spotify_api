e44cd831d4e009c3b0524dbd43ac1ec1f1884489
Jason Chen <cmjj8898212032@gmail.com>
2023-06-27 13:10:14 +0800

    feat: import react-query.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,27 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
 import styled from "styled-components";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 export default function Home() {
+  // Access the client
+  const queryClient = useQueryClient();
+
+  // Queries
+  // const query = useQuery({
+  //   queryKey: ["test"],
+  //   queryFn: async () => {
+  //     const res = await fetch("https://prompt-ashy-xi.vercel.app/api/prompt");
+  //     return res.json();
+  //   },
+  // });
+  // console.log(query);
+
   return <main></main>;
 }
